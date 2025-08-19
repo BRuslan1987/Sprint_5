2d0e449c913b1ee67c0cde26b1eff059d720c1a6
Руслан Батыров <bruslan.mail@yandex.ru>
2025-08-19 21:32:59 +0300

    Sprin_5, Проверка работы

---
+++ constants.py
@@ -0,0 +1,11 @@
+MAIN_URL = ('https://stellarburgers.nomoreparties.site/')
+
+LOGIN_PAGE = ('https://stellarburgers.nomoreparties.site/login')
+
+PROFILE_CABINET = ('https://stellarburgers.nomoreparties.site/account/profile') 
+
+EMAIL_FOR_LOGIN = ('ruslanbatyrov29012@yandex.ru')
+
+PASSWORD_FOR_LOGIN = ('Bruslan198')   
+
+NAME_FOR_REGISTRATION = ('Батыров Руслан')
